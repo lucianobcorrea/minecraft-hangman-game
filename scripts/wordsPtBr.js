a8005405b10a9d0bd173b32de1df529577d20cfc
Luciano Correa <lucianobalestrincorrea@gmail.com>
2025-06-03 22:50:21 -0300

    Feat: adicionando multilinguagem e possibilidade de digitar com teclado fisico

---
+++ scripts/wordsPtBr.js
@@ -0,0 +1,177 @@
+const wordsPtBr = [
+  {
+    id: 1,
+    name: "enderman",
+    category: "mob",
+  },
+  {
+    id: 2,
+    name: "creeper",
+    category: "mob",
+  },
+  {
+    id: 3,
+    name: "zumbi",
+    category: "mob",
+  },
+  {
+    id: 4,
+    name: "esqueleto",
+    category: "mob",
+  },
+  {
+    id: 5,
+    name: "aranha",
+    category: "mob",
+  },
+  {
+    id: 6,
+    name: "floresta",
+    category: "bioma",
+  },
+  {
+    id: 7,
+    name: "pântano",
+    category: "bioma",
+  },
+  {
+    id: 8,
+    name: "selva",
+    category: "bioma",
+  },
+  {
+    id: 9,
+    name: "taiga",
+    category: "bioma",
+  },
+  {
+    id: 10,
+    name: "deserto",
+    category: "bioma",
+  },
+  {
+    id: 11,
+    name: "pedra",
+    category: "bloco",
+  },
+  {
+    id: 12,
+    name: "terra",
+    category: "bloco",
+  },
+  {
+    id: 13,
+    name: "areia",
+    category: "bloco",
+  },
+  {
+    id: 14,
+    name: "tronco",
+    category: "bloco",
+  },
+  {
+    id: 15,
+    name: "gelo",
+    category: "bloco",
+  },
+  {
+    id: 16,
+    name: "machado",
+    category: "ferramentas",
+  },
+  {
+    id: 17,
+    name: "enxada",
+    category: "ferramentas",
+  },
+  {
+    id: 18,
+    name: "picareta",
+    category: "ferramentas",
+  },
+  {
+    id: 19,
+    name: "pá",
+    category: "ferramentas",
+  },
+  {
+    id: 20,
+    name: "tesoura",
+    category: "ferramentas",
+  },
+  {
+    id: 21,
+    name: "arco",
+    category: "armas",
+  },
+  {
+    id: 22,
+    name: "besta",
+    category: "armas",
+  },
+  {
+    id: 23,
+    name: "espada",
+    category: "armas",
+  },
+  {
+    id: 24,
+    name: "escudo",
+    category: "armas",
+  },
+  {
+    id: 25,
+    name: "flecha",
+    category: "armas",
+  },
+  {
+    id: 26,
+    name: "pão",
+    category: "alimentos",
+  },
+  {
+    id: 27,
+    name: "maçã",
+    category: "alimentos",
+  },
+  {
+    id: 28,
+    name: "bolo",
+    category: "alimentos",
+  },
+  {
+    id: 29,
+    name: "leite",
+    category: "alimentos",
+  },
+  {
+    id: 30,
+    name: "batata",
+    category: "alimentos",
+  },
+  {
+    id: 31,
+    name: "osso",
+    category: "materiais",
+  },
+  {
+    id: 32,
+    name: "pena",
+    category: "materiais",
+  },
+  {
+    id: 33,
+    name: "sílex",
+    category: "materiais",
+  },
+  {
+    id: 34,
+    name: "pólvora",
+    category: "materiais",
+  },
+  {
+    id: 35,
+    name: "papel",
+    category: "materiais",
+  },
+];
